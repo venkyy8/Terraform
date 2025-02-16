b9e2c75078337dc7a113da17d42cdc894e763e52
Venkateswara Reddy <113894770+venkyy8@users.noreply.github.com>
2025-02-16 22:45:36 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
   }
   backend "s3" {
     bucket         = "venkat1-new" 
-    key            = "global/s3/terraform.tfstate"    
+    key            = "custom-directory/terraform.tfstate"    
     region         = "ap-south-1"           
     encrypt        = true                   
     dynamodb_table = "terraform-backend"    
