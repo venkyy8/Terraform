aabb58a4cb0552ca32aa18837305cb5afae67cb3
venkyy82 <venkyy82@gmail.com>
2025-09-18 14:32:19 +0530

    restructured

---
+++ Basic-Infra-Setup/Ec2-Networking/README.md
@@ -0,0 +1,12 @@
+# overview of the file:
+
+## list of services we are creating:
+
+1. VPC
+2. subnet
+3. Internet Gateway
+4. Route Table
+   1. Route
+5. Route Table Association
+6. Security Group
+7. Ec2 instance
--- Modules_Directory/modules/actualmodulefiles/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-backend "s3" {
-    bucket         = "venkat1-new" 
-    key            = "global/s3/terraform.tfstate"    
-    region         = "ap-south-1"           
-    encrypt        = true                   
-    dynamodb_table = "s3-backend"    
-  }
-}
